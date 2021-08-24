da64fb0462546b3f3f413aee7e55d7b4e566f5dc
Levon Martirosyan1 <Levon_Martirosyan1@epamyerw016b.moscow.epam.com>
2021-08-24 16:32:36 +0400

    entrypoint changed

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,37 +3,41 @@ set -e
 
 if [ -n "$GITHUB_EVENT_PATH" ];
 then
-  EVENT_PATH=$GITHUB_EVENT_PATH
-elif [ -f /sample_push_event.json ];
+    EVENT_PATH=$GITHUB_EVENT_PATH
+elif [ -f ./sample_push_event.json ];
 then
-  EVENT_PATH='/sample_push_event.json'
-  LOCAL_TEST=true
+    EVENT_PATH='./sample_push_event.json'
+    LOCAL_TEST=true
 else
-  echo "No JSON data to process!"
-  exit 1
+    echo "No JSON data to process! :("
+    exit 1
 fi
 
 env
 jq . < $EVENT_PATH
 
+# if keyword is found
 if jq '.commits[].message, .head_commit.message' < $EVENT_PATH | grep -i -q "$*";
 then
-  VERSION=$(date +%F.%s)
+    # do something
+    VERSION=$(date +%F.%s)
 
-  DATA="$(printf '{"tag_name":"v%s",' $VERSION)"
-  DATA="${DATA} $(printf '"target_commitish":"master",')"
-  DATA="${DATA} $(printf '"name":"v%s",' $VERSION)"
-  DATA="${DATA} $(printf '"body":"Automated release based on keyword: %s",' "$*")"
-  DATA="${DATA} $(printf '"draft":false, "prerelease":false}')"
+    DATA="$(printf '{"tag_name":"v%s",' $VERSION)"
+    DATA="${DATA} $(printf '"target_commitish":"master",')"
+    DATA="${DATA} $(printf '"name":"v%s",' $VERSION)"
+    DATA="${DATA} $(printf '"body":"Automated release based on keyword: %s",' "$*")"
+    DATA="${DATA} $(printf '"draft":false, "prerelease":false}')"
 
-  URL="https://api.github.com/repos/${GITHUB_REPOSITORY}/releases?access_token=${GITHUB_TOKEN}"
+    URL="https://api.github.com/repos/${GITHUB_REPOSITORY}/releases?access_token=${GITHUB_TOKEN}"
 
-  if [[ "${LOCAL_TEST}" == *"true"* ]];
-  then
-    echo "## [TESTING] Keyword was found but no release was created."
-  else
-    echo $DATA | http POST $URL | jq .
-  fi
+    if [[ "${LOCAL_TEST}" == *"true"* ]];
+    then
+        echo "## [TESTING] Keyword was found but no release was created."
+    else
+        echo $DATA | http POST $URL | jq .
+    fi
+# otherwise
 else
-  echo "Nothing to process."
+    # exit gracefully
+    echo "Nothing to process."
 fi
